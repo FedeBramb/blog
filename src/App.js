b2ba9cd89273f778d76478a1eccf7acc8e7e65a7
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-05-24 21:30:40 +0200

    changehashrouter

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="">
       <div className='main_div'>
         <SocialBar position="header">
           <SearchBar />
