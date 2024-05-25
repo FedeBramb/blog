6eb2d9116caff75523c3a6e5248c70fe9c4cc3a4
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-05-25 13:32:10 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-
-// Componente principale che avvolge l'intera applicazione per fornire le funzionalità di routing.
-import { Routes, Route } from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import SocialBar from './Components/SocialBar/SocialBar.js';
 import SearchBar from './Components/SearchBar/SearchBar.js';
 import UpperBody from './Container/UpperBody/UpperBody.js';
@@ -15,50 +12,47 @@ import NewsBar from './Components/NewsBar/NewsBar.js';
 import CookBook from './Components/CookBook/CookBook.js';
 import Recipe from './Components/Recipe/Recipe.js';
 import Footer from './Container/Footer/Footer.js';
-
 import RenderIfVisible from 'react-render-if-visible';
-
 import './App.css';
-
 
 function App() {
   return (
-    
-      <div className='main_div'>
-        <SocialBar position="header">
-          <SearchBar />
-        </SocialBar>
-        {/*Search Bar*/}
-        <UpperBody>
-          {/*<Logo />*/}
-          <Navbar />
-        </UpperBody>
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Carousel />
-              <Middle>
-                <RenderIfVisible defaultHeight="78%" visibleOffset="50">
-                  <Center>
-                    <RecipesPrev />
-                    <NewsBar />
-                  </Center>
-                </RenderIfVisible>
-              </Middle></>
-          }>
-          </Route>
-          <Route path="/cookbook" element={
-            <CookBook/>}>
-          </Route>
-          <Route path="/cookbook/:recipeName" element={
-            <Recipe/>}>
-          </Route>
-        </Routes>
-        <Footer>
-          <SocialBar position="footer" />
-        </Footer>
-      </div>
-    
+    <div className='main_div'>
+      <SocialBar position="header">
+        <SearchBar />
+      </SocialBar>
+      <UpperBody>
+        <Navbar />
+      </UpperBody>
+      <Routes>
+        {/* Route per la home page */}
+        <Route path="/" element={<Navigate to="/blog" replace />} />
+
+        {/* Route per /blog */}
+        <Route path="/blog" element={
+          <>
+            <Carousel />
+            <Middle>
+              <RenderIfVisible defaultHeight="78%" visibleOffset="50">
+                <Center>
+                  <RecipesPrev />
+                  <NewsBar />
+                </Center>
+              </RenderIfVisible>
+            </Middle>
+          </>
+        } />
+
+        {/* Route per /blog/cookbook */}
+        <Route path="/blog/cookbook" element={<CookBook />} />
+
+        {/* Route per /blog/cookbook/:recipeName */}
+        <Route path="/blog/cookbook/:recipeName" element={<Recipe />} />
+      </Routes>
+      <Footer>
+        <SocialBar position="footer" />
+      </Footer>
+    </div>
   );
 }
 
