35d4f433a29e2815db3e86d722a47f6e829ffa2c
FedeBramb <146201363+FedeBramb@users.noreply.github.com>
2024-06-11 16:35:29 +0200

    update containers

--- src/hooks/getHeight.js
+++
@@ -1,8 +0,0 @@
-export function calculatePercentageHeight(element) {
-    const containerHeight = element.clientHeight; // Altezza dell'elemento container
-    const viewHeight = window.innerHeight; // Altezza della view
-  
-    const percentageHeight = ( viewHeight  / containerHeight ) * 100; // Calcolo della percentuale
-  
-    return percentageHeight;
-}
